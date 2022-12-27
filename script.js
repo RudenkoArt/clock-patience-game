2c3b98858287e23fb1c39e9b2bf2ba61201fafb5
RudenkoArt <rudnkoart@gmail.com>
2022-12-27 10:28:59 +0200

    Finished drag-n-drop functionality

--- script.js
+++ script.js
@@ -1,128 +1,33 @@
+const modal = document.querySelector('.modal');
 const piles = document.querySelectorAll('.pile');
-const borders = document.querySelectorAll('.border');
+const restart = document.getElementById('restart');
 
 // Game Conditions
+let isFirstGame = true;
 let totalCardsRevealed = 0;
 let kingsRevealed = 0;
 
 // Drag Functionality
-let previousPile;
+let currentPileId;
 let draggedItem;
 
-const cardsArray = [
-  '2_of_clubs',
-  '2_of_diamonds',
-  '2_of_hearts',
-  '2_of_spades',
-  '3_of_clubs',
-  '3_of_diamonds',
-  '3_of_hearts',
-  '3_of_spades',
-  '4_of_clubs',
-  '4_of_diamonds',
-  '4_of_hearts',
-  '4_of_spades',
-  '5_of_clubs',
-  '5_of_diamonds',
-  '5_of_hearts',
-  '5_of_spades',
-  '6_of_clubs',
-  '6_of_diamonds',
-  '6_of_hearts',
-  '6_of_spades',
-  '7_of_clubs',
-  '7_of_diamonds',
-  '7_of_hearts',
-  '7_of_spades',
-  '8_of_clubs',
-  '8_of_diamonds',
-  '8_of_hearts',
-  '8_of_spades',
-  '9_of_clubs',
-  '9_of_diamonds',
-  '9_of_hearts',
-  '9_of_spades',
-  '10_of_clubs',
-  '10_of_diamonds',
-  '10_of_hearts',
-  '10_of_spades',
-  'ace_of_clubs',
-  'ace_of_diamonds',
-  'ace_of_hearts',
-  'ace_of_spades',
-  'jack_of_clubs',
-  'jack_of_diamonds',
-  'jack_of_hearts',
-  'jack_of_spades',
-  'king_of_clubs',
-  'king_of_diamonds',
-  'king_of_hearts',
-  'king_of_spades',
-  'queen_of_clubs',
-  'queen_of_diamonds',
-  'queen_of_hearts',
-  'queen_of_spades',
-];
-
+// Cards
 // prettier-ignore
-const pilesNumber = { ace:1, jack:11, queen:12, king:13 };
-
-// function patience(cards) {
-//   const deck = [...cards];
-//   // prettier-ignore
-//   const cardValues = { A:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9,10:10, J:11, Q:12, K:13 };
-//   // prettier-ignore
-//   const table = { 1:[], 2:[], 3:[], 4:[], 5:[], 6:[], 7:[], 8:[], 9:[], 10:[], 11:[], 12:[], 13:[] };
-
-//   let i = 1;
-//   let fourKings = 0;
-//   let revealedCardPile = 13;
-
-//   while (deck.length > 0) {
-//     table[i] = table[i].concat(deck.shift());
-//     if (i === 13) {
-//       i = 1;
-//     } else i++;
-//   }
-
-//   while (fourKings < 4) {
-//     const revealedCard = table[revealedCardPile].pop();
-//     if (revealedCard === 'K') fourKings++;
-//     revealedCardPile = cardValues[revealedCard];
-//   }
-
-//   return Object.values(table).reduce((acc, arr) => (acc += arr.length), 0);
-// }
-
-/* <img class='back-side' src='./cards/10_of_clubs.png' alt='center' />; */
-
-// Drag n' Drop
-function dragStart(e) {
-  draggedItem = e.target;
-
-  makePileActive(draggedItem.id);
-
-  //   setTimeout(() => {
-  //     // draggedItem.hidden = true;
-  //   }, 10);
-}
-
-function allowDrop(e) {
-  e.preventDefault();
-}
-
-function drop(e) {
-  e.preventDefault();
-  const parentContainer = e.target.closest('div');
-  draggedItem.classList.add('played');
-  //   draggedItem.hidden = false;
-  parentContainer.appendChild(draggedItem);
-}
-
-function startGame() {
-  shuffleCardsArray(cardsArray);
-  populateDOM();
-}
+const cardsArray = [
+  '2_of_clubs', '2_of_diamonds', '2_of_hearts', '2_of_spades',
+  '3_of_clubs', '3_of_diamonds', '3_of_hearts', '3_of_spades',
+  '4_of_clubs', '4_of_diamonds', '4_of_hearts', '4_of_spades',
+  '5_of_clubs', '5_of_diamonds', '5_of_hearts', '5_of_spades',
+  '6_of_clubs', '6_of_diamonds', '6_of_hearts', '6_of_spades',
+  '7_of_clubs', '7_of_diamonds', '7_of_hearts', '7_of_spades',
+  '8_of_clubs', '8_of_diamonds', '8_of_hearts', '8_of_spades',
+  '9_of_clubs', '9_of_diamonds', '9_of_hearts', '9_of_spades',
+  '10_of_clubs', '10_of_diamonds', '10_of_hearts', '10_of_spades',
+  'jack_of_clubs', 'jack_of_diamonds', 'jack_of_hearts', 'jack_of_spades',
+  'queen_of_clubs', 'queen_of_diamonds', 'queen_of_hearts', 'queen_of_spades',
+  'king_of_clubs', 'king_of_diamonds', 'king_of_hearts', 'king_of_spades',
+  'ace_of_clubs', 'ace_of_diamonds', 'ace_of_hearts', 'ace_of_spades',
+];
 
 function shuffleCardsArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -131,44 +36,125 @@ function shuffleCardsArray(array) {
   }
 }
 
-function makePileActive(card) {
-  console.log(card);
-  const activePile = document.querySelector(
-    `.pile-${pilesNumber[card] ? pilesNumber[card] : card}`
-  );
-  activePile.style.zIndex = 100;
+// Drag n' Drop
+function dragStart(e) {
+  draggedItem = e.target;
+  setTimeout(() => {
+    makePileAvailible(draggedItem.id);
+    draggedItem.hidden = true;
+  }, 10);
+}
+
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+function drop(e) {
+  const parentContainer = e.target.closest('div');
+  draggedItem.classList.add('played');
+  draggedItem.setAttribute('draggable', 'false');
+  draggedItem.hidden = false;
+  parentContainer.appendChild(draggedItem);
+  currentPileId = parentContainer.id;
+}
+
+// Check Win/Loss Conditions
+function checkGameProgress() {
+  if (kingsRevealed === 4)
+    // create showModal function
+    setTimeout(() => (modal.style.display = 'flex'), 500);
 }
 
 function createCardEl(cardName) {
   const cardEl = document.createElement('img');
   cardEl.src = `./cards/${cardName}.png`;
   cardEl.id = cardName.slice(0, cardName.indexOf('_'));
-  cardEl.draggable = true;
+  cardEl.draggable = false;
   cardEl.setAttribute('ondragstart', 'dragStart(event)');
   cardEl.classList.add('back-side');
   cardEl.addEventListener('click', () => {
     if (cardEl.classList.contains('back-side')) {
       cardEl.classList.remove('back-side');
-      console.log(123);
+      cardEl.draggable = true;
+      if (cardEl.id === 'king') kingsRevealed++;
+      totalCardsRevealed++;
+      checkGameProgress();
     }
   });
   return cardEl;
 }
 
 function populateDOM() {
-  const cards = [...cardsArray];
+  // Clear Piles
+  if (!isFirstGame) piles.forEach((pile) => (pile.textContent = ''));
+  const cardEls = cardsArray.map((card) => createCardEl(card));
   piles.forEach((pile) => {
-    const stack = cards.splice(0, 4);
-    const cardEls = stack.map((card) => createCardEl(card));
-    cardEls.forEach((cardEl) => pile.firstElementChild.appendChild(cardEl));
+    pile.append(...cardEls.splice(0, 4));
   });
 }
 
+function setRootVarieble(name, value) {
+  return document.documentElement.style.setProperty(name, value);
+}
+
+// Trigger CSS transitions by changing root variables
+function dealCards() {
+  if (isFirstGame) {
+    setRootVarieble('--radius', '40vmin');
+    setRootVarieble('--r-offset', '-0.25turn');
+  } else {
+    setRootVarieble('--radius', '0');
+    setRootVarieble('--r-offset', '-1.75turn');
+    setTimeout(() => {
+      setRootVarieble('--radius', '40vmin');
+      setRootVarieble('--r-offset', '-0.25turn');
+    }, 1500);
+  }
+}
+
+function makePileAvailible(id) {
+  piles.forEach((pile) => {
+    pile.style.pointerEvents = pile.id === id ? '' : 'none';
+  });
+}
+
+function startGame() {
+  currentPileId = 'king';
+  makePileAvailible(currentPileId);
+  shuffleCardsArray(cardsArray);
+  populateDOM();
+  dealCards();
+  if (isFirstGame) isFirstGame = !isFirstGame;
+}
+
 // Event Listeners
-borders.forEach((border) => {
-  border.addEventListener('dragover', allowDrop);
-  border.addEventListener('drop', drop);
+piles.forEach((pile) => {
+  pile.addEventListener('dragover', allowDrop);
+  pile.addEventListener('drop', drop);
 });
+
+// When drag event was interrupted before drop()
+window.addEventListener('dragend', (e) => {
+  if (e.dataTransfer.dropEffect === 'none') {
+    makePileAvailible(currentPileId);
+    draggedItem.hidden = false;
+  }
+});
+
+restart.addEventListener('click', startGame);
 
 // On Load
 startGame();
+
+/* TODO: 
+- fix drop function on kings
+- add cards sounds
+- rewrite pile and card ids, since id is unique
+- disble restart btn during shuffle 
+- add borders for piles
+- add imgs inside of a pile border
+- add soliter win animation (waterfall)
+- add flip card animation
+- add UI counter and timer
+- light up needed pile +-
+*/
