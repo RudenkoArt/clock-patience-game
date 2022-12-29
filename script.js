02fd1b4d7a4d0d9672a2139a0a55565af7698a1a
RudenkoArt <rudnkoart@gmail.com>
2022-12-29 10:20:55 +0200

    Add timer, localStorage and modal window functionality

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
+const { body } = document;
 const modal = document.querySelector('.modal');
 const piles = document.querySelectorAll('.pile');
 const restart = document.getElementById('restart');
+const mute = document.getElementById('mute');
+const time = document.querySelector('.time');
+const bestTimeEl = document.getElementById('best-time');
 
 // Game Conditions
 let isFirstGame = true;
-let totalCardsRevealed = 0;
-let kingsRevealed = 0;
+let totalCardsMoved;
+let kingsRevealed;
+
+// Time
+let startingTime;
+let timer;
 
 // Drag Functionality
 let currentPileId;
 let draggedItem;
+
+// Sounds
+let isMuted = false;
+const cardFlipSound = new Audio('./sounds/card-flip.mp3');
+const cardDropSound = new Audio('./sounds/card-drop.mp3');
 
 // Cards
 // prettier-ignore
@@ -29,6 +42,7 @@ const cardsArray = [
   'ace_of_clubs', 'ace_of_diamonds', 'ace_of_hearts', 'ace_of_spades',
 ];
 
+// Shuffle Cards
 function shuffleCardsArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -36,11 +50,70 @@ function shuffleCardsArray(array) {
   }
 }
 
+// Check Win/Loss Conditions
+function checkGameProgress() {
+  if (kingsRevealed === 4) {
+    // Stop Timer
+    clearInterval(timer);
+    // Call makePileAvailible with no arguments to disable all piles
+    makePileAvailible();
+    setTimeout(
+      () => (totalCardsMoved === 52 ? showModal(true) : showModal(false)),
+      1500
+    );
+  }
+}
+
+// Show Notification Window
+function showModal(hasWon) {
+  // Clear modal window
+  modal.firstElementChild.textContent = '';
+  // Message
+  const messageEl = document.createElement('h3');
+  messageEl.textContent = hasWon ? '🎉 You Win! 🎉' : '😩 You Lost! 😩';
+  // Store Best Time
+  if (hasWon) storeBestTime();
+  // Time
+  const yourTimeEl = document.createElement('span');
+  yourTimeEl.textContent = `Your time: ${time.textContent.slice(-5)}`;
+  const bestTimeEl = document.createElement('span');
+  bestTimeEl.textContent = `Best time: ${localStorage.getItem('BestTime')}`;
+  // Break
+  const breakEl = document.createElement('br');
+  // Restart option
+  const restartEl = document.createElement('strong');
+  restartEl.textContent = hasWon ? 'Restart' : 'Try again';
+  restartEl.classList.add('play-again');
+  restartEl.setAttribute('onclick', 'startNewGame()');
+  // Append
+  modal.firstElementChild.append(
+    ...(hasWon
+      ? [messageEl, yourTimeEl, bestTimeEl, breakEl, restartEl]
+      : [messageEl, breakEl, restartEl])
+  );
+  // Store Best Time
+  // Show Modal
+  modal.style.display = 'flex';
+}
+
+// Timer
+function startTimer() {
+  startingTime = Date.now();
+  timer = setInterval(() => {
+    const diff = Date.now() - startingTime;
+    let s = Math.floor((diff / 1000) % 60);
+    let m = Math.floor(diff / 1000 / 60);
+    s = s < 10 ? `0${s}` : s;
+    m = m < 10 ? `0${m}` : m;
+    time.textContent = `Time: ${m}:${s}`;
+  }, 100);
+}
+
 // Drag n' Drop
 function dragStart(e) {
   draggedItem = e.target;
   setTimeout(() => {
-    makePileAvailible(draggedItem.id);
+    makePileAvailible(draggedItem.name);
     draggedItem.hidden = true;
   }, 10);
 }
@@ -56,22 +129,19 @@ function drop(e) {
   draggedItem.hidden = false;
   parentContainer.appendChild(draggedItem);
   currentPileId = parentContainer.id;
-}
-
-// Check Win/Loss Conditions
-function checkGameProgress() {
-  if (kingsRevealed === 4)
-    // create showModal function
-    setTimeout(() => (modal.style.display = 'flex'), 500);
+  if (draggedItem.name === 'king') kingsRevealed++;
+  totalCardsMoved++;
+  checkGameProgress();
+  if (!isMuted) cardDropSound.play();
 }
 
 // Create Playing Card
 function createCardEl(cardName) {
   const cardEl = document.createElement('img');
   cardEl.src = `./cards/${cardName}.png`;
-  cardEl.id = cardName.slice(0, cardName.indexOf('_'));
   cardEl.draggable = false;
   cardEl.setAttribute('ondragstart', 'dragStart(event)');
+  cardEl.setAttribute('name', cardName.slice(0, cardName.indexOf('_')));
   cardEl.classList.add('back-side');
   cardEl.addEventListener('click', () => {
     if (cardEl.classList.contains('back-side')) {
@@ -79,9 +149,7 @@ function createCardEl(cardName) {
       // Add .flipping to trigger the animation
       cardEl.classList.add('flipping');
       cardEl.draggable = true;
-      if (cardEl.id === 'king') kingsRevealed++;
-      totalCardsRevealed++;
-      checkGameProgress();
+      if (!isMuted) cardFlipSound.play();
     }
     // Remove .flipping to prevent further animation triggers
     cardEl.addEventListener('animationend', () => {
@@ -91,32 +159,57 @@ function createCardEl(cardName) {
   return cardEl;
 }
 
-//
+// Append Card Elements into Piles
 function populateDOM() {
   // Clear Piles
   if (!isFirstGame) piles.forEach((pile) => (pile.textContent = ''));
   const cardEls = cardsArray.map((card) => createCardEl(card));
-  piles.forEach((pile) => {
+  piles.forEach((pile, idx) => {
+    // Start timer when first Card is clicked
+    if (idx === 12)
+      cardEls[3].addEventListener('click', function () {
+        startTimer();
+        // Remove this event immediately after click
+        this.removeEventListener('click', arguments.callee);
+      });
     pile.append(...cardEls.splice(0, 4));
   });
 }
 
+// Trigger transitions by changing CSS root variables
 function setRootVarieble(name, value) {
   return document.documentElement.style.setProperty(name, value);
 }
 
-// Trigger CSS transitions by changing root variables
+function firstDealAnimation() {
+  setRootVarieble('--radius', '40vmin');
+  setRootVarieble('--r-offset', '-0.25turn');
+}
+
+function nextDealAnimation() {
+  setRootVarieble('--radius', '0');
+  setRootVarieble('--r-offset', '-1.75turn');
+  // Disable page clickability during transitions
+  body.style.pointerEvents = 'none';
+  setTimeout(() => {
+    body.style.pointerEvents = '';
+  }, 3000);
+  // Set Event Listener to one of the piles
+  piles[0].addEventListener('transitionend', nextDealAnimationEnd);
+}
+
+function nextDealAnimationEnd() {
+  setRootVarieble('--radius', '40vmin');
+  setRootVarieble('--r-offset', '-0.25turn');
+  // Remove Event Listener
+  piles[0].removeEventListener('transitionend', nextDealAnimationEnd);
+}
+
 function dealCards() {
   if (isFirstGame) {
-    setRootVarieble('--radius', '40vmin');
-    setRootVarieble('--r-offset', '-0.25turn');
+    firstDealAnimation();
   } else {
-    setRootVarieble('--radius', '0');
-    setRootVarieble('--r-offset', '-1.75turn');
-    setTimeout(() => {
-      setRootVarieble('--radius', '40vmin');
-      setRootVarieble('--r-offset', '-0.25turn');
-    }, 1500);
+    nextDealAnimation();
   }
 }
 
@@ -126,12 +219,44 @@ function makePileAvailible(id) {
   });
 }
 
-function startGame() {
+function muteSounds() {
+  isMuted = !isMuted;
+  if (isMuted) {
+    mute.classList.replace('fa-volume-high', 'fa-volume-xmark');
+    mute.title = 'Unmute';
+  } else {
+    mute.classList.replace('fa-volume-xmark', 'fa-volume-high');
+    mute.title = 'Mute';
+  }
+}
+
+// Store and get Best Time
+function checkBestTime() {
+  bestTimeEl.textContent = localStorage.getItem('BestTime')
+    ? `Best Time: ${localStorage.getItem('BestTime')}`
+    : 'Best Time: --:--';
+}
+
+function storeBestTime() {
+  const currentTime = time.textContent.slice(-5);
+  const bestTime = localStorage.getItem('BestTime');
+  if (!bestTime || bestTime > currentTime)
+    localStorage.setItem('BestTime', currentTime);
+}
+
+// Start Game
+function startNewGame() {
   currentPileId = 'king';
+  totalCardsMoved = 51;
+  kingsRevealed = 4;
+  modal.style.display = 'none';
+  clearInterval(timer);
+  time.textContent = 'Time: 00:00';
   makePileAvailible(currentPileId);
   shuffleCardsArray(cardsArray);
   populateDOM();
   dealCards();
+  checkBestTime();
   if (isFirstGame) isFirstGame = !isFirstGame;
 }
 
@@ -149,7 +274,11 @@ window.addEventListener('dragend', (e) => {
   }
 });
 
-restart.addEventListener('click', startGame);
+// Restart game
+restart.addEventListener('click', startNewGame);
+
+// Sounds Off/On
+mute.addEventListener('click', muteSounds);
 
 // On Load
-startGame();
+startNewGame();
